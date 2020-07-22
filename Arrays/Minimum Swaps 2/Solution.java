d396ec53b8e8bd2bceaa3d239168905213ed163a
Nick Zelada <nickzelada@ymail.com>
2020-07-21 21:38:55 -0400

    Update Solution.java

--- Arrays/Minimum Swaps 2/Solution.java
+++ Arrays/Minimum Swaps 2/Solution.java
@@ -6,11 +6,10 @@ public class Solution {
 
     // Complete the minimumSwaps function below.
       static int minimumSwaps(int[] array) {
-        int n = array.length - 1;
         int minSwaps = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             if (i < array[i] - 1) {
-                swap(array, i, Math.min(n, array[i] - 1));
+                swap(array, i, Math.min(array.length - 1, array[i] - 1));
                 minSwaps++;
                 i--;
             }
@@ -24,13 +23,12 @@ public class Solution {
         array[j] = temp;
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
        int[] newInt = new int[] {1,3,5,2,4,6,7};
-       System.out.println();
-       System.out.println("The number of swaps are: "+minimumSwaps(newInt));
-       System.out.println();
+      
+       System.out.println("\nThe number of swaps are: "+minimumSwaps(newInt)+"\n");
+     
     }
       
 }
