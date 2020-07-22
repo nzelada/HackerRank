3f34fee57f07df8ad8bdf66cf1fe18ada42821ac
Nick Zelada <nickzelada@ymail.com>
2020-07-21 21:51:13 -0400

    Update Solution.java

--- Arrays/Minimum Swaps 2/Solution.java
+++ Arrays/Minimum Swaps 2/Solution.java
@@ -6,28 +6,28 @@ public class Solution {
 
     // Complete the minimumSwaps function below.
       static int minimumSwaps(int[] array) {
-        int minSwaps = 0;
-        for (int i = 0; i < array.length - 1; i++) {
-            if (i < array[i] - 1) {
-                swap(array, i, Math.min(array.length - 1, array[i] - 1));
-                minSwaps++;
-                i--;
+        int minSwaps = 0; // Start at 0
+        for (int i = 0; i < array.length - 1; i++) { // run loop until one less of size of array
+            if (i < array[i] - 1) { // if i is less than the number - 1 then
+                swap(array, i, Math.min(array.length - 1, array[i] - 1)); // call for swap, the array, i, smallest # 
+                minSwaps++; // count
+                i--; // decrease
             }
         }
-        return minSwaps;
+        return minSwaps; // return it
     }
 
     private static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        int temp = array[i]; // swap 
+        array[i] = array[j]; // swap  
+        array[j] = temp; // swap 
     }
 
 
     public static void main(String[] args) throws IOException {
        int[] newInt = new int[] {1,3,5,2,4,6,7};
       
-       System.out.println("\nThe number of swaps are: "+minimumSwaps(newInt)+"\n");
+       System.out.println("\nThe number of swaps are: "+minimumSwaps(newInt)+"\n"); // call and print
      
     }
       
